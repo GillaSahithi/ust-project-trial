236bf65390cda5327df318d85eb3e98f82a34eee
9182092380 <pothulasharath452@gmail.com>
2024-02-22 19:58:02 +0530

    secoundchange

---
+++ src/main/java/com/example/sharathproject/controller/Sharathcontroller.java
@@ -0,0 +1,15 @@
+package com.example.sharathproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sharathcontroller {
+
+    @GetMapping("/ust")
+    public String hello(){
+        return "hi how are you?";
+    }
+
+
+}
